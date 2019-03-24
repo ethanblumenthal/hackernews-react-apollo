f10e4802e7c53c38b75d27243d79c4e12a4551bc
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-24 11:44:54 -0500

    added realtime updates with graphql subscriptions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -23,6 +26,30 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : ''
     }
   }
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
 })
 
 const client = new ApolloClient({
